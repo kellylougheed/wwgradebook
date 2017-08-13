d12c426f9b43c184bd746f2dc5171493627f2248
Kelly Lougheed <kellylougheed@gmail.com>
2017-08-13 15:54:39 +0000

    Tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :students, only: [:create]
     resources :assignments, only: [:create, :new]
   end
-  resources :students, only: [:show, :edit, :update, :destroy]
+  resources :students, only: [:show, :edit, :update, :destroy] # do
+    # resources :comments, only: [:create, :edit, :update, :destroy]
+    # resources :entries, only: [:create, :edit, :update, :destroy]
+  # end
   resources :assignments, only: [:show, :edit, :update, :destroy]
 end
